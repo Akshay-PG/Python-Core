d26649b54a49a297928c46f6f7dee01e9e8cf4f3
Akshay Puliyangot <130630569+Akshay-PG@users.noreply.github.com>
2023-06-23 14:35:07 +0530

    Add files via upload

---
+++ list functions.py
@@ -0,0 +1,19 @@
+fruits = ['mango','apple','orange','pineapple']
+
+#append
+fruits.append("banana")
+print(fruits)
+
+veg = ['carrot','chilli','brinjal']
+fruits.append(veg)
+print(fruits)
+
+#length
+print(len(fruits))
+
+#insert
+fruits.insert(1,'grape')
+print(fruits)
+
+#index
+print(fruits.index('grape'))
