e29cf52758c4e311ba99889df087b818ef0b7d32
Akshay Puliyangot <130630569+Akshay-PG@users.noreply.github.com>
2023-06-23 11:48:10 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,31 @@
+
+#LIST OPERATIONS
+
+abc = ['google','fb','watsap','linkedin']
+print(abc)
+print(abc[1])
+
+no= [1,2,3,4,5]
+print(no[4])
+
+no[2] = 5
+print(no)
+
+num = [7,8,9,10]
+
+print(no+num)
+
+efg = ['tb','mb','gb']
+print(abc+efg)
+
+hij=(efg+abc)
+print(hij)
+
+hij[1]='sd'
+print(hij)
+
+print(num*3)
+
+print('tb' in hij)
+
+
