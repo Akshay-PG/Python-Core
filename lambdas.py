98f46e993e63653cdc125bf0900f225d5c19ef02
Akshay Puliyangot <130630569+Akshay-PG@users.noreply.github.com>
2023-06-25 16:04:29 +0530

    Add files via upload

---
+++ lambdas.py
@@ -0,0 +1,13 @@
+# this is a normal function
+def function1(a):
+    return a+10
+
+print(function1(20))
+
+#lambda function
+
+print((lambda a:a+10) (20))
+
+res = (lambda a:a+10) (20)
+print(res)
+
